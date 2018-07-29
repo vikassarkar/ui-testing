c4f33711a3659de6c9ca73d1d52e8e833dbcbc6d
vikassarkar <vikas.sarkar2014@gmail.com>
2018-07-29 13:55:18 +0530

    update for testing setup

--- e2e-testing-react-redux-with-protractor-jasmine/tests/config.js
+++ e2e-testing-react-redux-with-protractor-jasmine/tests/config.js
@@ -1,71 +1,99 @@
-// exports.config = {
-//     frameworks: "mocha", // Use mocha instead of jasmine
-//     //directConnect: true, // selenium will not need a server, direct connet to chrome
-//     seleniumAddress: "http://localhost:4444/wd/hub",
-//     baseUrl: "http://localhost:6606",
-//     specs: [// pattern for the test specs
-//         "e2e/*spec.js",
-//         "e2e/**/*spec.js"
-//     ],
-//     // beforeLaunch: function () { // Do all the typescript 
-//     //     require("ts-node").register({
-//     //         project: "e2e/tsconfig.e2e.json"
-//     //     });
-//     // },
-//     onPrepare: () => {
-//         browser.ignoreSynchronization = true
-//         // var width = 2250
-//         // var height = 1200
-//         // browser.driver.manage().window().setSize(width, height)
-//         // require("babel-core/register")
-//         // require("./setup.js")
-//     },
-//     mochaOpts: { // Mocha specific options 
-//         reporter: "spec",
-//         slow: 3000,
-//         ui: "bdd",
-//         timeout: 30000
-//     },
-//     capabilities: {
-//         "browserName": "firefox"
-//     },
-//     allScriptsTimeout: 15000, // The timeout for a script run on the browser. 
-// }
 
-const { SpecReporter } = require("jasmine-spec-reporter");
-// const path = require("path");
-// const PrettyReporter = require("protractor-pretty-html-reporter").Reporter;
-// const sanityReporter = new PrettyReporter({
-//     path: path.join(__dirname, "../__report__/for-management"),
-//     screenshotOnPassed: true
-// });
+const path = require("path");
+const fs = require("fs");
+const fse = require("fs-extra");
+const VideoReporter = require("protractor-video-reporter");
+VideoReporter.prototype.jasmineStarted = function () {
+    let self = this;
+    if (self.options.singleVideo) {
+        const videoPath = path.join(self.options.baseDirectory, "protractor-specs.avi");
+        self._startScreencast(videoPath);
+        if (self.options.createSubtitles) {
+            self._subtitles = [];
+            self._jasmineStartTime = new Date();
+        }
+    }
+};
+const videoReport = new VideoReporter({
+    baseDirectory: path.join(__dirname, "../__report__/__video-report__"),
+    saveSuccessVideos: true,
+    createSubtitles: true,
+    singleVideo: true,
+    ffmpegCmd: path.normalize("./node_modules/ffmpeg-binaries/bin/ffmpeg.exe"),
+    ffmpegArgs: [
+        "-f", "gdigrab",
+        "-framerate", "24",
+        "-video_size", "wsxga",
+        "-i", "desktop",
+        "-q:v", "10",
+    ]
+});
+const PrettyReporter = require("protractor-pretty-html-reporter").Reporter;
+const sanityReport = new PrettyReporter({
+    path: path.join(__dirname, "../__report__/__mgmt-report__"),
+    screenshotOnPassed: true,
+    highlightSuspectLine: true
+});
+const SpecReporter = require("jasmine-spec-reporter").SpecReporter;
+const specReport = new SpecReporter({
+    displayFailuresSummary: true,
+    displayFailuredSpec: true,
+    displaySuiteNumber: true,
+    displaySpecDuration: true
+});
+const createReportsFolder = function () {
+    const reportDir = [path.join(__dirname, "../__report__/__mgmt-report__"), path.join(__dirname, "../__report__/__dev-report__"), path.join(__dirname, "../__report__/__video-report__")]
+    if (fs.existsSync(path.join(__dirname, "../__report__"))) {
+        fse.removeSync(path.join(__dirname, "../__report__"));
+    }
+    for (let dir of reportDir) {
+        if (!fs.existsSync(dir)) {
+            fse.ensureDirSync(dir);
+        }
+    }
+}();
+
+
+//module.exports = function(options) {
+const myReporter = function() {
+    const reporter = [specReport, sanityReport, videoReport];
+    if (reporter) {
+      (Array.isArray(reporter) ? reporter : [reporter]).forEach(function (el) {
+        jasmine.addReporter(el);
+      });
+    }else{
+        jasmine.addReporter(specReport);
+    }
+  };
+
 
 exports.config = {
     allScriptsTimeout: 11000,
     framework: "jasmine",
     directConnect: true, //lets Protractor connect directly to the browser drivers only for chrome and firefox
-    seleniumAddress: "http://localhost:4444/wd/hub",
+    //seleniumAddress: "http://localhost:4444/wd/hub",
     baseUrl: "http://localhost:6606",
     specs: [
         "e2e/*spec.js",
         "e2e/**/*spec.js"
     ],
     multiCapabilities: [
-        //     {
+        {
+            "browserName": "chrome",
+            "version": "ANY",
+            "chromeOptions": {
+                "args": ["--start-maximized", "--always-authorize-plugins"],
+            }
+        },
+        // {
         //     "browserName": "firefox",
         //     "moz:firefoxOptions": {
         //         "args": ["--safe-mode"]
         //     },
         //     "version": "ANY",
         //     "firefoxPath": "C:/Program Files (x86)/Mozilla Firefox/firefox.exe"
-        // }, 
-        {
-            "browserName": "chrome",
-            "version": "ANY",
-            "chromeOptions": {
-                "args": ["--start-maximized"],
-            }
-        }],
+        // }
+    ],
     // capabilities: {
     //     "browserName": "internet explorer",
     //     "version": "ANY"
@@ -73,16 +101,16 @@ exports.config = {
     // localSeleniumStandaloneOpts: {
     //     jvmArgs: ["-Dwebdriver.ie.driver=C:\Users\vikassar\AppData\Roaming\nvm\v8.9.4\node_modules\webdriver-manager\selenium\IEDriverServer3.13.0.exe"] // e.g: "node_modules/protractor/node_modules/webdriver-manager/selenium/IEDriverServer_x64_X.XX.X.exe"
     // },
-    // plugins: [{
-    //     package: "protractor-screenshoter-plugin",
-    //     screenshotPath: "./__report__/for-developers",
-    //     screenshotOnExpect: "failure+success",
-    //     screenshotOnSpec: "none",
-    //     withLogs: true,
-    //     writeReportFreq: "asap",
-    //     imageToAscii: "none",
-    //     clearFoldersBeforeTest: true
-    // }],
+    plugins: [{
+        package: "protractor-screenshoter-plugin",
+        screenshotPath: path.join(__dirname, "../__report__/__dev-report__"),
+        screenshotOnExpect: "failure+success",
+        screenshotOnSpec: "none",
+        withLogs: true,
+        writeReportFreq: "asap",
+        imageToAscii: "none",
+        clearFoldersBeforeTest: true
+    }],
     jasmineNodeOpts: {
         "showColors": true,
         "defaultTimeoutInterval": 30000,
@@ -101,12 +129,11 @@ exports.config = {
      * You can specify a file containing code to run by setting beforeLaunch to
      * the filename string.
      */
-    // beforeLaunch: function () { // Do all the typescript 
-    //     sanityReporter.startReporter();
-    //     //     require("ts-node").register({
-    //     //         project: "e2e/tsconfig.e2e.json"
-    //     //     });
-    // },
+    beforeLaunch: function () { // Do all the typescript 
+        //     require("ts-node").register({
+        //         project: "e2e/tsconfig.e2e.json"
+        //     });
+    },
     /**
      * A callback function called once protractor is ready and available, and
      * before the specs are executed. If multiple capabilities are being run,
@@ -115,7 +142,10 @@ exports.config = {
     onPrepare: () => {
         browser.driver.manage().window().maximize();
         browser.ignoreSynchronization = true;
-        jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+        require("babel-core/register"); //required for es6
+        //require("./setup.js");
+        //jasmine.getEnv().addReporter(specReport, sanityReport, videoReport)
+        jasmine.getEnv().addReporter(specReport)
     },
     /**
      * A callback function called once all tests have finished running and
@@ -152,5 +182,4 @@ exports.config = {
      * The path is relative to the location of this config.
      */
     resultJsonOutputFile: "./__report__/report.json"
-
-}
+};
--- e2e-testing-react-redux-with-protractor-jasmine/tests/setup.js
+++
@@ -1,9 +0,0 @@
-const optimist = require('optimist');
-import * as chai from 'chai';
-import * as chaiAsPromised from 'chai-as-promised';
-
-chai.use(chaiAsPromised);
-global.chai = chai;
-global.chaiAsPromised = chaiAsPromised
-global.expect = chai.expect;
-global.optimist = optimist;
